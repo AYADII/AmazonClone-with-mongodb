d3805b22d7d8ab0f7abfd986cad597571867a84c
AYADII <yassinayadi44@gmail.com>
2022-10-27 11:08:43 +0100

    first commit

---
+++ ECommerce/frontend/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <header>
+       <a href="/">Amazon</a>
+        </header>
+        <main> list product </main>
+        
+    </div>
+  );
+}
+
+export default App;
